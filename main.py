3c0e2cf372e369f1768630a7036a1f541186a6ee
hkaanengin <hkaanengin@windowslive.com>
2024-02-29 22:44:41 +0300

    fix

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     try:
         conn = psycopg2.connect("host=localhost dbname=votingDB user=kekuser password=kekpwd")
         cur = conn.cursor()
-        postgre_create_tables()
+        postgre_create_tables(conn, cur)
 
         cur.execute("""
             SELECT * FROM candidates
